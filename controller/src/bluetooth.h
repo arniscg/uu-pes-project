9208ada9ca78e0a5b01aed13637098ee15d0fa6f
Arnis Gustins <arniscgustins@gmail.com>
2022-04-28 17:25:24 +0200

    bt cleanup

---
+++ controller/src/bluetooth.h
@@ -0,0 +1,88 @@
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/conn.h>
+#include <bluetooth/uuid.h>
+#include <bluetooth/gatt.h>
+#include <bluetooth/services/hrs.h>
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA_BYTES(BT_DATA_UUID16_ALL,
+		      BT_UUID_16_ENCODE(BT_UUID_HRS_VAL),
+		      BT_UUID_16_ENCODE(BT_UUID_DIS_VAL))
+};
+
+static void connected(struct bt_conn *conn, uint8_t err)
+{
+	if (err) {
+		printk("Connection failed (err 0x%02x)\n", err);
+	} else {
+		printk("Connected\n");
+	}
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	printk("Disconnected (reason 0x%02x)\n", reason);
+}
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.connected = connected,
+	.disconnected = disconnected,
+};
+
+static void bt_ready(void)
+{
+	int err;
+
+	printk("Bluetooth initialized\n");
+
+	err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
+	if (err) {
+		printk("Advertising failed to start (err %d)\n", err);
+		return;
+	}
+
+	printk("Advertising successfully started\n");
+}
+
+static void auth_cancel(struct bt_conn *conn)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	printk("Pairing cancelled: %s\n", addr);
+}
+
+static struct bt_conn_auth_cb auth_cb_display = {
+	.cancel = auth_cancel,
+};
+
+/**
+ * Function that initialized everything related with bluetooth
+ */
+static int connect_bluetooth() {
+    int err = bt_enable(NULL);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return 0;
+	}
+
+	bt_ready();
+
+	bt_conn_auth_cb_register(&auth_cb_display);
+
+    return 1;
+}
+
+/**
+ * Function that sends ambient sensor value to basestation
+ */
+static void send_light_sensor_value(uint16_t value) {
+	printk("Sending ambient light value %d lux to basestation\n", value);
+	// Using predefined heartrate sensor bluetooth service
+	// We will have to create our own service in the future
+	// This only sends 8bit data but we will need at least 16bit to support values above 255
+	bt_hrs_notify(value);
+}
--- controller/src/main.c
+++ controller/src/main.c
@@ -1,11 +1,3 @@
-/* main.c - Application main entry point */
-
-/*
- * Copyright (c) 2015-2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <zephyr/types.h>
 #include <stddef.h>
 #include <string.h>
@@ -17,84 +9,19 @@
 #include <drivers/sensor.h>
 #include <stdio.h>
 
-#include <bluetooth/bluetooth.h>
-#include <bluetooth/hci.h>
-#include <bluetooth/conn.h>
-#include <bluetooth/uuid.h>
-#include <bluetooth/gatt.h>
-#include <bluetooth/services/hrs.h>
-
-static const struct bt_data ad[] = {
-	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-	BT_DATA_BYTES(BT_DATA_UUID16_ALL,
-		      BT_UUID_16_ENCODE(BT_UUID_HRS_VAL),
-		      BT_UUID_16_ENCODE(BT_UUID_DIS_VAL))
-};
-
-static void connected(struct bt_conn *conn, uint8_t err)
-{
-	if (err) {
-		printk("Connection failed (err 0x%02x)\n", err);
-	} else {
-		printk("Connected\n");
-	}
-}
-
-static void disconnected(struct bt_conn *conn, uint8_t reason)
-{
-	printk("Disconnected (reason 0x%02x)\n", reason);
-}
-
-BT_CONN_CB_DEFINE(conn_callbacks) = {
-	.connected = connected,
-	.disconnected = disconnected,
-};
-
-static void bt_ready(void)
-{
-	int err;
-
-	printk("Bluetooth initialized\n");
-
-	err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
-	if (err) {
-		printk("Advertising failed to start (err %d)\n", err);
-		return;
-	}
-
-	printk("Advertising successfully started\n");
-}
-
-static void auth_cancel(struct bt_conn *conn)
-{
-	char addr[BT_ADDR_LE_STR_LEN];
-
-	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
-
-	printk("Pairing cancelled: %s\n", addr);
-}
-
-static struct bt_conn_auth_cb auth_cb_display = {
-	.cancel = auth_cancel,
-};
+#include "bluetooth.h"
 
 void main(void)
 {
-	int err;
-
-	err = bt_enable(NULL);
-	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
+	// Initialize bluetooth connection
+	int bt_okay = connect_bluetooth();
+	if (!bt_okay) {
+		printf("Failed to connect to bluetooth");
 		return;
 	}
 
-	bt_ready();
-
-	bt_conn_auth_cb_register(&auth_cb_display);
-
 	// Initialize the light sensor
 	const struct device *veml7700 = device_get_binding("VEML7700");
-
 	if (veml7700 == NULL) {
 		printf("No device \"%s\" found; did initialization fail?\n", "VEML7700");
 		return;
@@ -102,16 +29,15 @@ void main(void)
 
 	struct sensor_value lux;
 
+	// Main loop
 	while (1) {
 		k_sleep(K_SECONDS(1));
 
+		// Get sensor reading
 		sensor_sample_fetch(veml7700);
 		sensor_channel_get(veml7700, SENSOR_CHAN_LIGHT, &lux);
-		printf("Sensor reading: %dlux\n", lux.val1);
 
-		// Using predefined heartrate sensor bluetooth service
-		// We will have to create our own service in the future
-		// This only sends 8bit data but we will need at least 16bit to support values above 255
-		bt_hrs_notify(lux.val1);
+		// Send to basestation
+		send_light_sensor_value(lux.val1);
 	}
 }
